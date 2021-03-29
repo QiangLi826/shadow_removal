12b159c187a0f060df85074f8236ef143167e481
dragonQiang <a625846910@163.com>
2021-03-29 15:43:41 +0800

    输出文件路径

---
+++ out_file_path.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+# In[2]:
+
+
+if __name__ == '__main__':
+
+    try:
+        f = open("test.txt", "w+")
+
+        def getallfile(inputpath,outputpath):
+            allfilelist = os.listdir(inputpath)
+            for file in allfilelist:
+                filepath = os.path.join(inputpath, file)
+                # 判断是不是文件夹
+                if os.path.isdir(filepath):
+                    getallfile(filepath, os.path.join(outputpath, file))
+                else:
+                    if (filepath.lower().endswith(
+                            ('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff'))):
+                        #print(filepath)
+                        f.write(filepath + ' ' + filepath + '\n')
+
+
+
+        getallfile("input", "output")
+    finally:
+        f.close()
+
+
+
+
+
+
+
